1b3b4f605b88cdea958443d617b3e349da74a027
Unathi Koketso Skosana <19790457@sun.ac.za>
2018-11-14 20:50:48 +0200

    Add shebang to shell scripts

--- scripts/histograms.sh
+++ scripts/histograms.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 for filename in $1/*.dat; do
     [ -e "$filename" ] || continue
 		/usr/bin/python3 $HOME/CP344/scripts/histograms.py $filename
--- scripts/jackknife.sh
+++ scripts/jackknife.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 for filename in $1/*.dat; do
     [ -e "$filename" ] || continue
 		$HOME/CP344/src/ising --jk $filename $2
